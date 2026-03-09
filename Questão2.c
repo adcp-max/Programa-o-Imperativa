71e0a1712e2fa312db4edf4abf15c02881bfdf9b
adcp-max <adcp@cin.ufpe.br>
2026-03-09 18:02:12 -0300

    Update Questão2.c

--- Questão2.c
+++ Questão2.c
@@ -2,6 +2,42 @@
 
 int main (){
 
+    long long a, b, n;
+    scanf("%lld", &n);
+    scanf("%lld %lld", &a, &b);
+
+    long long somaFinal, somaN, somaMultiplosA, somaMultiplosB, qtdMultiploA, qtdMultiploB, qtdMultiplosAB, intersec;
+    
+    somaN = n*(n+1)/2;
+    
+    qtdMultiploA = n/a;
+    
+    somaMultiplosA = ((a + qtdMultiploA*a)*qtdMultiploA)/2;
+    
+    qtdMultiploB = n/b;
+    
+    somaMultiplosB = ((b + qtdMultiploB*b)*qtdMultiploB)/2;
+    
+    qtdMultiplosAB = n/(a*b);
+    
+    intersec = ((a*b + qtdMultiplosAB*a*b)*qtdMultiplosAB)/2;
+    
+    somaFinal = somaN - somaMultiplosA - somaMultiplosB + intersec;
+    
+    printf("%lld\n", somaFinal);
+    
+    
+    if (somaFinal % 2 == 0){
+        printf("L� ele!!!");
+    }else{
+        printf("Opa xupenio AULAS...");
+    }
+    
+    return 0;
+}#include <stdio.h>
+
+int main (){
+
     int a, b, n;
     scanf("%d", &n);
     scanf("%d %d", &a, &b);
