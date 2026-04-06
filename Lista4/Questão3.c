95cb2d498386f25e851d4e782ea7eee30f187d31
adcp-max <adcp@cin.ufpe.br>
2026-04-05 21:10:37 -0300

    Create Questão3.c

---
+++ Lista4/Questão3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+    int yatual=0, xatual=0, matriz[4][4] = {{0, 1, 1, 0}, {1, 2, 5, 2}, {1, 1, 2, 1}, {0, 1, 1, 1}};
+    char n;
+
+    for(int k=0;k<20;k++){
+        scanf("%c", &n);
+        if (n=='c'){
+            yatual+=1;
+            matriz[yatual][xatual] +=1;
+        }else if(n=='b'){
+            yatual-=1;
+            matriz[yatual][xatual] +=1;
+        }else if(n=='d'){
+            xatual+=1;
+            matriz[yatual][xatual] +=1;
+        }else if(n=='e'){
+            xatual-=1;
+            matriz[yatual][xatual] +=1;
+        }
+    }
+
+    int maiorValor=0, maiorX=0, maiorY=0;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(matriz[i][j]>maiorValor){
+                maiorX = j;
+                maiorY = i;
+                maiorValor= matriz[i][j];
+            }
+        }
+    }
+
+    printf("Coordenada X: %d, Y: %d", maiorX, maiorY);
+
+    return 0;
+}
